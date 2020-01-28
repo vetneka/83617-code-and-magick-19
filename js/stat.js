4bbf5d80463fbb8a195a323a453ae9e4417a8e51
vetneka <44982805+vetneka@users.noreply.github.com>
2020-01-29 01:04:03 +0400

    Убрал излишнюю валидацию, избавился от 'чужого' решения

--- js/stat.js
+++ js/stat.js
@@ -8,9 +8,8 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_PADDING_TOP = 15;
 var CLOUD_PADDING_LEFT = 25;
 var CLOUD_PADDING_BOTTOM = 20;
-
-var TITLE_MAX_WIDTH = 200;
-var TITLE_LINE_HEIGHT = 20;
+var CLOUD_COLOR = '#fff';
+var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7';
 
 var HISTOGRAM_X = CLOUD_X + CLOUD_PADDING_LEFT;
 var HISTOGRAM_Y = CLOUD_HEIGHT - CLOUD_PADDING_BOTTOM;
@@ -21,6 +20,8 @@ var BAR_MAX_WIDTH = 40;
 var BAR_GAP = 50;
 var BAR_COLOR_CURRENT_PLAYER = 'rgba(255, 0, 0, 1)';
 
+var BASE_FONT = '16px PT Mono';
+var TEXT_LINE_HEIGHT = 20;
 var TEXT_COLOR = '#000';
 
 /**
@@ -31,6 +32,7 @@ var TEXT_COLOR = '#000';
  * @param {number} x - start x-coordinate of cloud
  * @param {number} y - start y-coordinate of cloud
  * @param {string} color - color of cloud
+ * @return {void}
  */
 var cloudRender = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -39,51 +41,22 @@ var cloudRender = function (ctx, x, y, color) {
 
 /**
  * @description
- *  Function for automatically wrap long text
- *
- * @param {HTMLCanvasElement} ctx - context '2d'
- * @param {string} text - Long (or any) text
- * @param {number} marginLeft - x-positioning of text in canvas
- * @param {number} marginTop - y-positioning of text in canvas
- * @param {number} maxWidth - max-width after which the text is wrap
- * @param {number} lineHeight - y-gap between lines
- */
-var wrapText = function (ctx, text, marginLeft, marginTop, maxWidth, lineHeight) {
-  var words = text.split(' ');
-  var countWords = words.length;
-  var line = '';
-
-  for (var n = 0; n < countWords; n++) {
-    var testLine = line + words[n] + ' ';
-    var testWidth = ctx.measureText(testLine).width;
-
-    if (testWidth > maxWidth) {
-      ctx.fillText(line, marginLeft, marginTop);
-      line = words[n] + ' ';
-      marginTop += lineHeight;
-    } else {
-      line = testLine;
-    }
-  }
-
-  ctx.fillText(line, marginLeft, marginTop);
-};
-
-/**
- * @description
- *  Render title on canvas
+ *  Render text on canvas
  *
  * @param {HTMLCanvasElement} ctx - context '2d'
  * @param {string} text - text for title
+ * @param {number} x - x-positioning of text in canvas
+ * @param {number} y - y-positioning of text in canvas
  * @param {string} font - font property for text, syntax CSS font
  * @param {string} color - text color
+ * @return {void}
  */
-var titleRender = function (ctx, text, font, color) {
+var textRender = function (ctx, text, x, y, font, color) {
   ctx.fillStyle = color;
   ctx.font = font;
   ctx.textBaseline = 'hanging';
 
-  wrapText(ctx, text, CLOUD_X + CLOUD_PADDING_LEFT, CLOUD_Y + CLOUD_PADDING_TOP, TITLE_MAX_WIDTH, TITLE_LINE_HEIGHT);
+  ctx.fillText(text, x, y);
 };
 
 /**
@@ -94,11 +67,11 @@ var titleRender = function (ctx, text, font, color) {
  * @return {number} - max number in array
  */
 var getMaxNumber = function (array) {
-  var maxNumber = +array[0];
+  var maxNumber = array[0];
 
   for (var i = 1; i < array.length; i++) {
-    if (+array[i] > maxNumber) {
-      maxNumber = +array[i];
+    if (array[i] > maxNumber) {
+      maxNumber = array[i];
     }
   }
 
@@ -113,6 +86,7 @@ var getMaxNumber = function (array) {
  * @param {number} height - histogram column height
  * @param {string} color - histogram column color
  * @param {number} gap - gap between columns
+ * @return {void}
  */
 var paintHistogramColumn = function (ctx, height, color, gap) {
   ctx.fillStyle = color;
@@ -138,24 +112,36 @@ var paintHistogramColumn = function (ctx, height, color, gap) {
  * @param {number} y - start y-coordinate render histogram
  * @param {array} names - array players
  * @param {array} times - time of passing the level of the corresponding player from the "names" array
+ * @return {void}
  */
 var histogramRender = function (ctx, x, y, names, times) {
   var maxTime = getMaxNumber(times);
 
   for (var i = 0; i < names.length; i++) {
     var barHeight = BAR_MAX_HEIGHT * times[i] / maxTime;
+    var currentPlayerName = names[i];
+    var currentPlayerTime = Math.round(times[i]);
+
+    var startTextX = (CLOUD_X + CLOUD_PADDING_LEFT) + ((BAR_MAX_WIDTH + BAR_GAP) * i);
+    var startNamesY = HISTOGRAM_Y;
+    var startTimesY = HISTOGRAM_Y - barHeight - 35;
 
     if (names[i] === 'Вы') {
       paintHistogramColumn(ctx, barHeight, BAR_COLOR_CURRENT_PLAYER, ((BAR_MAX_WIDTH + BAR_GAP) * i));
     } else {
       var barSaturation = Math.floor(Math.random() * 100);
       var barColor = 'hsl(240, ' + barSaturation + '%, 50%)';
-      paintHistogramColumn(ctx, barHeight, barColor, ((BAR_MAX_WIDTH + BAR_GAP) * i));
+      var histogramColumnGap = ((BAR_MAX_WIDTH + BAR_GAP) * i);
+
+      paintHistogramColumn(ctx, barHeight, barColor, histogramColumnGap);
     }
 
-    ctx.fillStyle = TEXT_COLOR;
-    ctx.fillText(names[i], (CLOUD_X + CLOUD_PADDING_LEFT) + ((BAR_MAX_WIDTH + BAR_GAP) * i), HISTOGRAM_Y);
-    ctx.fillText(Math.round(times[i]), (CLOUD_X + CLOUD_PADDING_LEFT) + ((BAR_MAX_WIDTH + BAR_GAP) * i), HISTOGRAM_Y - barHeight - 35);
+    /* ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(currentPlayerName, startTextX, startNamesY);
+    ctx.fillText(currentPlayerTime, startTextX, startTimesY); */
+
+    textRender(ctx, currentPlayerName, startTextX, startNamesY, BASE_FONT, TEXT_COLOR);
+    textRender(ctx, currentPlayerTime, startTextX, startTimesY, BASE_FONT, TEXT_COLOR);
   }
 };
 
@@ -166,12 +152,21 @@ var histogramRender = function (ctx, x, y, names, times) {
  * @param {HTMLCanvasElement} ctx - context '2d'
  * @param {array} names - array players
  * @param {array} times - time of passing the level of the corresponding player from the "names" array
+ * @return {void}
  */
 window.renderStatistics = function (ctx, names, times) {
-  cloudRender(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7');
-  cloudRender(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  cloudRender(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, CLOUD_SHADOW_COLOR);
+  cloudRender(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-  titleRender(ctx, 'Ура вы победили! Список результатов:', '16px PT Mono', '#000');
+  /* ctx.fillStyle = TEXT_COLOR;
+  ctx.font = BASE_FONT;
+  ctx.textBaseline = 'hanging';
+
+  ctx.fillText('Ура вы победили!', CLOUD_X + CLOUD_PADDING_LEFT, CLOUD_Y + CLOUD_PADDING_TOP);
+  ctx.fillText('Список результатов:', CLOUD_X + CLOUD_PADDING_LEFT, CLOUD_Y + CLOUD_PADDING_TOP + TEXT_LINE_HEIGHT); */
+
+  textRender(ctx, 'Ура вы победили!', CLOUD_X + CLOUD_PADDING_LEFT, CLOUD_Y + CLOUD_PADDING_TOP, BASE_FONT, TEXT_COLOR);
+  textRender(ctx, 'Список результатов:', CLOUD_X + CLOUD_PADDING_LEFT, CLOUD_Y + CLOUD_PADDING_TOP + TEXT_LINE_HEIGHT, BASE_FONT, TEXT_COLOR);
 
   histogramRender(ctx, HISTOGRAM_X, HISTOGRAM_Y, names, times);
 };
