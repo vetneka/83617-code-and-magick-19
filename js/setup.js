c023abc9967b91c1a02bdbbad3d368dacdd284d2
vetneka <44982805+vetneka@users.noreply.github.com>
2020-02-01 17:24:34 +0400

    Исправил код в соответствии со стандартом ES5, переписал функцию получения случайных чисел

--- js/setup.js
+++ js/setup.js
@@ -46,69 +46,51 @@ var setupSimilar = document.querySelector('.setup-similar');
 setupSimilar.classList.remove('hidden');
 var setupSimilarList = document.querySelector('.setup-similar-list');
 
-var similarWizardTemplate = document.querySelector("#similar-wizard-template").content.querySelector(".setup-similar-item");
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+var setupSimilarItem = similarWizardTemplate.querySelector('.setup-similar-item');
 
 /**
  * @description
- *  Getting random number in range from 0 to range
+ *  Getting random number in range from begin to end
  *
- * @param {number} [range=10] - max number in selected range
- * @param {number} [multiplier=10] - multiplier for scaling the result Math.random()
+ * @param {number} [begin=0] - min number in selected range
+ * @param {number} [end=10] - max number in selected range
  *
  * @return {number} random number
  */
-var getRandomNumber = function (range = 10, multiplier = 10) {
-  return Math.floor(Math.random() * multiplier) % range;
-}
+var getRandomNumber = function (begin, end) {
+  var startNumber = begin || 0;
+  var endNumber = end || 10;
+
+  var result;
+  var counter = 1;
+  var digit = 10;
+
+  for (var i = 10; i <= endNumber; i *= 10) {
+    counter++;
+  }
+
+  digit = Math.pow(digit, counter);
+
+  result = Math.round(Math.random() * digit);
+
+  while (result < startNumber || result > endNumber) {
+    result = Math.round(Math.random() * digit);
+  }
+
+  return result;
+};
 
 /**
  * @description
- *  Getting a random value from an array
+ *  Get a random value from an array
  *
- * @param {array} array - any array from which you want to get a random value
- * @return {any} - random value from the array
+ * @param {array} array - an array from which to get a random value
+ * @return {any}
  */
 var getRandomArrayValue = function (array) {
-  /* // var №1
-  var randomIndex = Math.floor(Math.random() * 10);
-  var randomValue = array[randomIndex];
-
-  if (randomValue === undefined) {
-    randomValue = array[0];
-  }
-
-  return randomValue;
-  */
-
-  // var №2
-  var randomValue;
-  var randomIndex;
-  var randomIndexArray = [];
-
-  var roundedArrayLength;
-  var numberBitCounter = 0;
-
-  if (array.length < 10) {
-    numberBitCounter = 1;
-  }
-
-  for (var i = 10; i <= array.length; i *= 10) {
-    numberBitCounter++;
-  }
-
-  //roundedArrayLength = 10 ** numberBitCounter;
-  roundedArrayLength = Math.pow(10, numberBitCounter);
-
-  for (var i = 10; i <= roundedArrayLength; i *= 10) {
-    randomIndex = getRandomNumber(array.length, i);
-    randomIndexArray.push(randomIndex);
-  }
-
-  randomIndex = getRandomNumber(randomIndexArray.length, roundedArrayLength);
-  randomValue = array[randomIndexArray[randomIndex]];
-
-  return randomValue;
-}
+  return array[getRandomNumber(0, array.length - 1)];
+};
 
 /**
  * @description
@@ -135,10 +117,9 @@ var createSimilarWizard = function () {
  * @return {object} - wizard HTML-markup
  */
 var renderSimilarWizard = function (wizard) {
-  var elementWizard = similarWizardTemplate.cloneNode(true);
+  var elementWizard = setupSimilarItem.cloneNode(true);
   var setupSimilarLabel = elementWizard.querySelector('.setup-similar-label');
   setupSimilarLabel.textContent = wizard.name + ' ' + wizard.surname;
-  //setupSimilarLabel.textContent = `${wizard.name} ${wizard.surname}`;
 
   elementWizard.querySelector('.wizard-coat').setAttribute('fill', wizard.coatColor);
   elementWizard.querySelector('.wizard-eyes').setAttribute('fill', wizard.eyesColor);
@@ -150,10 +131,11 @@ var renderSimilarWizard = function (wizard) {
  * @description
  *  Create fragment containing HTML-markup for n-th wizards
  *
- * @param {number} [numberOfWizards=4] - numbers of wizards
+ * @param {number} [number=4] - numbers of wizards
  * @return {object} - fragment containing HTML-markup
  */
-var renderSimilarWizards = function (numberOfWizards = 4) {
+var renderSimilarWizards = function (number) {
+  var numberOfWizards = number || 4;
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < numberOfWizards; i++) {
@@ -165,4 +147,6 @@ var renderSimilarWizards = function (numberOfWizards = 4) {
   return fragment;
 };
 
-setupSimilarList.appendChild(renderSimilarWizards());
+var similarWizards = renderSimilarWizards();
+
+setupSimilarList.appendChild(similarWizards);
