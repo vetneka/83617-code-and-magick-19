c22fec97df6350eb6835ced4b25d3e68586a3956
vetneka <44982805+vetneka@users.noreply.github.com>
2020-02-07 11:38:06 +0400

    feat(dialog) - add dialog movement

--- js/dialog.js
+++ js/dialog.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var SETUP_START_X = 50 + '%';
+  var SETUP_START_Y = 80 + 'px';
   var setup = document.querySelector('.setup');
 
   var setupSimilar = document.querySelector('.setup-similar');
@@ -24,6 +26,8 @@
   var openSetup = function () {
     setup.classList.remove('hidden');
     document.addEventListener('keydown', onKeydownEscSetup);
+    setup.style.left = SETUP_START_X;
+    setup.style.top = SETUP_START_Y;
   };
 
   /**
@@ -59,5 +63,46 @@
 
   setupUserName.addEventListener('blur', function () {
     document.addEventListener('keydown', onKeydownEscSetup);
+  });
+
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    var startX = evt.clientX;
+    var startY = evt.clientY;
+
+    var isDraggable = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      isDraggable = true;
+
+      var shiftX = moveEvt.clientX - startX;
+      var shiftY = moveEvt.clientY - startY;
+
+      startX = moveEvt.clientX;
+      startY = moveEvt.clientY;
+
+      setup.style.top = (setup.offsetTop + shiftY) + 'px';
+      setup.style.left = (setup.offsetLeft + shiftX) + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      if (isDraggable) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
