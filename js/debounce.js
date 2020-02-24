3be5ecb48e0fe0aed7e793e621a2b17aa4632286
vetneka <44982805+vetneka@users.noreply.github.com>
2020-02-24 19:59:02 +0400

    feat(debounce.js) - add debounce module

---
+++ js/debounce.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function () {
+  var DEBOUNCE_INTERVAL = 500;
+
+  var debounce = function (action) {
+    var lastTimeout = null;
+
+    return function () {
+      var parameters = arguments;
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        action.apply(null, parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+
+  window.debounce = {
+    set: debounce,
+  }
+})();
